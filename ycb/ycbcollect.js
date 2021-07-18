41e6d691175b7655d2a0d1b0621ea1104e1c0f78
16571681707 <77574453+16571681707@users.noreply.github.com>
2021-07-18 15:37:30 +0800

    Update ycbcollect.js

--- ycb/ycbcollect.js
+++ ycb/ycbcollect.js
@@ -2,15 +2,7 @@ var fetch = require('node-fetch');
 
 const url = "http://api.yongchenbao.com/api/Member/getCoin";
 const method = "POST";
-const headers = { "Authorization": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjbGllbnQueGhhZG1pbiIsImF1ZCI6InNlcnZlci54aGFkbWluIiwiaWF0IjoxNjI2MzIxNjY0LCJleHAiOjE2NDM1MjE2NjQsInVpZCI6IjE4NzQ5In0.iyksrMn6FHLIAPN0tDYlJ5jvO1gem5u9pdamVwFUVJY",
-    "user-agent": "UNI-APP/1.0",
-    "Content-Type": "application/x-www-form-urlencoded; charset\u003dutf-8",
-    "Content-Length": "24",
-    "Host": "api.yongchenbao.com",
-    "Connection": "Keep-Alive",
-    "Accept-Encoding": "gzip",
-    "Cookie": "PHPSESSID\u003da6547cf348b75ad98aa1583bf1c0e8de"
-};
+const headers = process.env.YCB_COLLECT;
 const myurl = {
     method: "POST",
     headers: headers,
